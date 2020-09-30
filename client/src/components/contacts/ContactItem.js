e3eae888ccddf5dc45db1654258a42cb0b9a331f
Keegan <keegan.adams15@gmail.com>
2020-09-29 19:25:52 -0500

    started contacts

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const contactItem = ({ contact }) => {
+  const { id, name, email, phone, type } = contact;
+
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}{" "}
+        <span
+          className={
+            "badge " + (type === "personal" ? "badge-primary" : "badge-success")
+          }
+        >
+          {type}
+        </span>
+      </h3>
+    </div>
+  );
+};
+
+export default contactItem;
